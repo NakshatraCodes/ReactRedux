4f63fba06183d05182c4f355ffeeaa65901abe38
NakshatraCodes <nakshatra163@gmail.com>
2021-02-24 23:59:37 +0530

    Redux Init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <PostForm />
-        <Posts />
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <PostForm />
+          <Posts />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
